e8b381a85941b329713f0d2fe1d0ced1f188c9a5
Laurent Smet <las@openerp.com>
2018-01-11 17:07:58 +0100

    [RENAME] report_intrastat -> account_intrastat module

--- addons/account_intrastat/tests/test_report_intrastat.py
+++ addons/account_intrastat/tests/test_report_intrastat.py
@@ -10,7 +10,7 @@ from odoo.modules.module import get_module_resource
 class RepoortIntrastatTest(common.TransactionCase):
 
     def _load(self, module, *args):
-        tools.convert_file(self.cr, 'report_intrastat',
+        tools.convert_file(self.cr, 'account_intrastat',
                            get_module_resource(module, *args),
                            {}, 'init', False, 'test', self.registry._assertion_report)
 
@@ -24,11 +24,11 @@ class RepoortIntrastatTest(common.TransactionCase):
             'partner_id': self.ref('base.res_partner_1'),
             'state': 'draft',
             'type': 'out_invoice',
-            'account_id': self.ref('report_intrastat.a_recv'),
+            'account_id': self.ref('account_intrastat.a_recv'),
             'name': 'Test invoice 1'
         })
 
     def test_00_create_pdf(self):
-        data, data_format = self.env.ref('report_intrastat.account_intrastatinvoices').render(self.invoice.ids)
+        data, data_format = self.env.ref('account_intrastat.account_intrastatinvoices').render(self.invoice.ids)
         if tools.config['test_report_directory']:
             open(os.path.join(tools.config['test_report_directory'], 'report_intrastat-intrastat_report.' + data_format), 'wb+').write(data)
