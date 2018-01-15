f22d7cba34ba01c1dd31964cee4ff09a73b58242
Xavier Morel <xmo@odoo.com>
2018-01-15 15:02:10 +0100

    [ADD] auth_password_policy: config

---
+++ addons/auth_password_policy/__manifest__.py
@@ -0,0 +1,6 @@
+{
+    'name': "Password Policy",
+    "summary": "Implements basic password policy configuration & check",
+    'depends': ['base_setup'],
+    'data': ['views/res_config_settings_views.xml']
+}
