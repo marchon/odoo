e16715d30d96051580bf3398d98b7ca1cdb63b1e
Olivier Colson <oco@odoo.com>
2018-01-16 15:30:14 +0100

    - draft version of complete management of invoicing>shipment for SO

--- addons/sale_stock/models/stock.py
+++ addons/sale_stock/models/stock.py
@@ -3,6 +3,8 @@
 
 from odoo import api, fields, models
 
+from odoo.tools.float_utils import float_is_zero
+
 
 class StockLocationRoute(models.Model):
     _inherit = "stock.location.route"
@@ -43,12 +45,87 @@ class StockMove(models.Model):
                         line.qty_delivered = line._get_delivered_qty()
         return res
 
+    def _prepare_account_move_line(self, qty, cost, credit_account_id, debit_account_id): #TODO OCO c'est ici dedans qu'il va falloir bidouiller, je pense !
+        #TODO OCO DOC override
+        res = super(StockMove, self)._prepare_account_move_line(qty, cost, credit_account_id, debit_account_id)
+
+        if self._is_out() and self.company_id.anglo_saxon_accounting and self._get_related_invoices():
+
+            product_accounts = self.product_id.product_tmpl_id._get_product_accounts()
+            interim_output_account = product_accounts['stock_output']
+            interim_output_invoice_move_lines = self.env['account.move.line'].search([('move_id', 'in', self._get_related_invoices().mapped('move_id.id')), ('account_id', '=', interim_output_account.id)])
+
+            # TODO OCO recherche de la valeur de débit de ce stock move
+            debit_val = None
+            for res_line in res:
+                line_data = res_line[2]
+                if res_line['account_id'] == interim_output_account.id:
+                    debit_val = line_data['debit']
+                    break
+
+            if debit_val == None:
+                raise UserError(_("No debit valuation in interim account"))
+
+            #TODO OCO Calcul du montant rectificatif à écrire
+            previous_stock_moves = self.env['stock.move'].search([('state', '=', 'done'), ('group_id', '=' , self.group_id)])
+            previously_shipped_qty = sum(previous_stock_moves.mapped('product_qty'))
+            sorted_invoice_move_lines = interim_output_invoice_move_lines.sorted(key=lambda x: x.date)
+            difference_with_invoice = 0.0
+            qty_to_treat = self.product_qty
+            for invoice_aml in sorted_invoice_move_lines:
+
+                invoice_aml_qty_left = 0
+                if not float_is_zero(previously_shipped_qty, precision_rounding=self.product_id.uom_id.rounding):
+                    qty_to_sbustract = min(invoice_aml.quantity, previously_shipped_qty)
+                    previously_shipped_qty -= qty_to_substract
+                    invoice_aml_qty_left = invoice_aml.quantity - qty_to_substract
+
+                if invoice_aml_qty_left:
+                    treated_qty = min(qty_to_treat, invoice_aml_qty_left)
+                    difference_with_invoice += (invoice_aml.balance / invoice_aml.quantity) * treated_qty
+                    qty_to_treat -= treated_qty
+
+                if float_is_zero(qty_to_treat, precision_rounding=self.product_id.uom_id.rounding):
+                    break
+
+            # TODO OCO génération des données d'aml supplémentaires
+
+            if not self.company_id.currency_id.is_zero(difference_with_invoice):
+                balancing_output_line_vals = {
+                    'name': _('Effective inventory valuation correction'),
+                    'product_id': self.product_id.id,
+                    'quantity': qty,
+                    'product_uom_id': self.product_id.uom_id.id,
+                    'ref': self.picking_id.name,
+                    'partner_id': partner_id,
+                    'credit': difference_with_invoice > 0 and difference_with_invoice or 0,
+                    'debit': difference_with_invoice < 0 and -difference_with_invoice or 0,
+                    'account_id': interim_output_account.id,
+                }
+
+                balancing_output_counterpart_line_vals = {
+                    'name': _('Effective inventory valuation correction'),
+                    'product_id': self.product_id.id,
+                    'quantity': qty,
+                    'product_uom_id': self.product_id.uom_id.id,
+                    'ref': self.picking_id.name,
+                    'partner_id': partner_id,
+                    'credit': difference_with_invoice < 0 and -difference_with_invoice or 0,
+                    'debit': difference_with_invoice > 0 and difference_with_invoice or 0,
+                    'account_id': product_accounts['expense'].id,
+                }
+
+                res.append((0, 0, balancing_output_line_vals))
+                res.append((0, 0, balancing_output_counterpart_line_vals))
+
+        return res
+
     def _get_related_invoices(self):
         """ Overridden from stock_account to return the customer invoices
         related to this stock move.
         """
         rslt = super(StockMove, self)._get_related_invoices()
-        rslt += self.picking_id.sale_id.invoice_ids
+        rslt += self.picking_id.sale_id.invoice_ids.filtered(lambda x: x.state not in ('draft', 'cancel'))
         return rslt
 
 
