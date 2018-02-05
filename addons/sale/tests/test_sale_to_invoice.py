a14989ad53825fe29bfbef55900741176e02c610
Hiral Bhavsar <hbh@odoo.com>
2018-02-05 19:08:40 +0530

    [WIP] test_sale_to_invoice

--- addons/sale/tests/test_sale_to_invoice.py
+++ addons/sale/tests/test_sale_to_invoice.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 from odoo.tools import mute_logger
 from .test_sale_common import TestCommonSaleNoChart
+from odoo.tests import Form, tagged
 
 
 class TestSaleToInvoice(TestCommonSaleNoChart):
@@ -12,63 +13,94 @@ class TestSaleToInvoice(TestCommonSaleNoChart):
     @classmethod
     def setUpClass(cls):
         super(TestSaleToInvoice, cls).setUpClass()
-        cls.setUpAdditionalAccounts()
-        cls.setUpAccountJournal()
+        # cls.setUpAdditionalAccounts()
+        # cls.setUpAccountJournal()
+        cls.setUpClassicProducts()
 
-    @mute_logger('odoo.addons.base.models.ir_model', 'odoo.osv.orm')
-    def setUp(self):
-        super(TestSaleToInvoice, self).setUp()
+        # create SO with 4 products
+        order_form = Form(cls.env['sale.order'])
+        order_form.partner_id = cls.partner_customer_usd
+        with order_form.order_line.new() as line:
+            line.product_id = cls.product_order
+        with order_form.order_line.new() as line:
+            line.product_id = cls.service_deliver
+        with order_form.order_line.new() as line:
+            line.product_id = cls.service_order
+        with order_form.order_line.new() as line:
+            line.product_id = cls.product_deliver
+        cls.sale_order = order_form.save()
 
-    def test_sale_to_invoice(self):
-        """ Testing for invoice create,validate and pay with invoicing and payment user."""
-        # Usefull models
-        IrModelData = self.env['ir.model.data']
-        partner_obj = self.env['res.partner']
-        journal_obj = self.env['account.journal']
-        # Usefull record id
-        group_id = IrModelData.xmlid_to_res_id('account.group_account_invoice') or False
-        company_id = IrModelData.xmlid_to_res_id('base.main_company') or False
 
-        # Usefull accounts
-        account_recv_id = self.account_receivable
-        account_rev_id = self.account_revenue
+    def test_downpayment(self):
+        """ Test downpayment """
+        # confirm SO
+        self.sale_order.action_confirm()
+        context = {
+            "active_model": 'sale.order',
+            "active_ids": [self.sale_order.id],
+            "active_id": self.sale_order.id,
+            'open_invoices': True,
+        }
+        payment_form = Form(self.env['sale.advance.payment.inv'].with_context(context))
+        payment_form.advance_payment_method = 'fixed'
+        payment_form.amount = 100
+        payment = payment_form.save()
+        invoice = payment.create_invoices()
 
-        # Add account to product
-        product_template_id = self.env.ref('sale.advance_product_0').product_tmpl_id
-        product_template_id.write({'property_account_income_id': account_rev_id})
+    # @mute_logger('odoo.addons.base.models.ir_model', 'odoo.osv.orm')
+    # def setUp(self):
+    #     super(TestSaleToInvoice, self).setUp()
 
-        # In order to test, I create new user and applied Invoicing & Payments group.
-        user = self.env['res.users'].create({
-            'name': 'Test User',
-            'login': 'test@test.com',
-            'company_id': 1,
-            'groups_id': [(6, 0, [group_id])]})
-        assert user, "User will not created."
-        # I create partner for sales order.
-        partner = partner_obj.create({
-            'name': 'Test Customer',
-            'email': 'testcustomer@test.com',
-            'property_account_receivable_id': account_recv_id,
-        })
+    # def test_sale_to_invoice(self):
+    #     """ Testing for invoice create,validate and pay with invoicing and payment user."""
+    #     # Usefull models
+    #     IrModelData = self.env['ir.model.data']
+    #     partner_obj = self.env['res.partner']
+    #     journal_obj = self.env['account.journal']
+    #     # Usefull record id
+    #     group_id = IrModelData.xmlid_to_res_id('account.group_account_invoice') or False
+    #     company_id = IrModelData.xmlid_to_res_id('base.main_company') or False
 
-        # In order to test I create sales order and confirmed it.
-        order = self.env['sale.order'].create({
-            'partner_id': partner.id,
-            'partner_invoice_id': partner.id,
-            'partner_shipping_id': partner.id,
-            'date_order': datetime.today(),
-            'pricelist_id': self.env.ref('product.list0').id})
-        assert order, "Sales order will not created."
-        context = {"active_model": 'sale.order', "active_ids": [order.id], "active_id": order.id}
-        order.with_context(context).action_confirm()
-        # Now I create invoice.
-        payment = self.env['sale.advance.payment.inv'].create({
-            'advance_payment_method': 'fixed',
-            'amount': 5,
-            'product_id': self.env.ref('sale.advance_product_0').id,
-        })
-        invoice = payment.with_context(context).create_invoices()
-        assert order.invoice_ids, "No any invoice is created for this sales order"
-        # Now I validate pay invoice wihth Test User(invoicing and payment).
-        for invoice in order.invoice_ids:
-            invoice.with_context(context).invoice_validate()
+    #     # Usefull accounts
+    #     account_recv_id = self.account_receivable
+    #     account_rev_id = self.account_revenue
+
+    #     # Add account to product
+    #     product_template_id = self.env.ref('sale.advance_product_0').product_tmpl_id
+    #     product_template_id.write({'property_account_income_id': account_rev_id})
+
+    #     # In order to test, I create new user and applied Invoicing & Payments group.
+    #     user = self.env['res.users'].create({
+    #         'name': 'Test User',
+    #         'login': 'test@test.com',
+    #         'company_id': 1,
+    #         'groups_id': [(6, 0, [group_id])]})
+    #     assert user, "User will not created."
+    #     # I create partner for sales order.
+    #     partner = partner_obj.create({
+    #         'name': 'Test Customer',
+    #         'email': 'testcustomer@test.com',
+    #         'property_account_receivable_id': account_recv_id,
+    #     })
+
+    #     # In order to test I create sales order and confirmed it.
+    #     order = self.env['sale.order'].create({
+    #         'partner_id': partner.id,
+    #         'partner_invoice_id': partner.id,
+    #         'partner_shipping_id': partner.id,
+    #         'date_order': datetime.today(),
+    #         'pricelist_id': self.env.ref('product.list0').id})
+    #     assert order, "Sales order will not created."
+    #     context = {"active_model": 'sale.order', "active_ids": [order.id], "active_id": order.id}
+    #     order.with_context(context).action_confirm()
+    #     # Now I create invoice.
+    #     payment = self.env['sale.advance.payment.inv'].create({
+    #         'advance_payment_method': 'fixed',
+    #         'amount': 5,
+    #         'product_id': self.env.ref('sale.advance_product_0').id,
+    #     })
+    #     invoice = payment.with_context(context).create_invoices()
+    #     assert order.invoice_ids, "No any invoice is created for this sales order"
+    #     # Now I validate pay invoice wihth Test User(invoicing and payment).
+    #     for invoice in order.invoice_ids:
+    #         invoice.with_context(context).invoice_validate()
